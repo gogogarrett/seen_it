eac5805eab63f8bd8c02037718c260129d2156c9
Garrett Heinlen <heinleng@gmail.com>
2014-03-03 19:20:01 +1100

    Showing basic _seen movies_ tab.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,13 @@
 class MoviesController < UIViewController
 
+  def init
+    super
+    self.title = "Seen movies"
+    self.tabBarItem = UITabBarItem.alloc.initWithTabBarSystemItem(UITabBarSystemItemTopRated, tag: 1)
+    self
+  end
+
   def viewDidLoad
+    view.backgroundColor = UIColor.scrollViewTexturedBackgroundColor
   end
 end
