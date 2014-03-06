904e1c1023b1d40bd3ea5822664dba2cb1a2c8c6
Garrett Heinlen <heinleng@gmail.com>
2014-03-06 21:45:31 +1100

    updating the listing of movies to list all seen movies.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,20 +3,40 @@ class MoviesController < UIViewController
   def init
     super
     self.title = "Seen movies"
-    self.tabBarItem = UITabBarItem.alloc.initWithTabBarSystemItem(UITabBarSystemItemTopRated, tag: 1)
+    self.tabBarItem.image = UIImage.imageNamed('seen_it_icon.png')
     self
   end
 
-  def viewDidLoad
-    view.backgroundColor = UIColor.scrollViewTexturedBackgroundColor
+  def loadView
+    self.view = UIScrollView.new
+    self.view.contentSize = CGSizeMake(320, 800)
+  end
+
+  def viewWillAppear(animated)
     add_seen_movies
+  end
+
+  def viewDidLoad
+    view.backgroundColor = UIColor.whiteColor
   end
 
   def add_seen_movies
     @movies = []
-    @movies = App::Persistence['movies'].uniq.map{|x| Movie.new(x) }
+    if App::Persistence['movies']
+      @movies = App::Persistence['movies'].uniq.map{|x| Movie.new(x) }.uniq
+    end
 
     @movies.each_with_index do |movie, index|
+
+      # I feel like this is hacky.. There should be a better way to do this?..
+      should_skip = false
+      view.subviews.each do |view|
+        if view.is_a?(UILabel) && view.text == movie.title
+          should_skip = true
+        end
+      end
+      next if should_skip
+
       label = UILabel.new
       label.text = movie.title
       label.lineBreakMode = UILineBreakModeWordWrap;
@@ -24,14 +44,15 @@ class MoviesController < UIViewController
 
       if !@last_label.nil?
         label.frame = [
-          [0, @last_label.frame.origin.y + 50],
+          [20, @last_label.frame.origin.y + 50],
           [250, 50]
         ]
       else
-        label.frame = [[0, 100], [250, 50]]
+        label.frame = [[20, 30], [250, 50]]
       end
 
       @last_label = label
+
       view.addSubview(label)
     end
   end
