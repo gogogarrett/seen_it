42698704efd0923335b871e9b74336bbe38d25ea
Garrett Heinlen <heinleng@gmail.com>
2014-03-15 23:14:17 +1100

    fixing silly bug.

--- app/views/detail_view.rb
+++ app/views/detail_view.rb
@@ -9,9 +9,9 @@ class DetailView < UIScrollView
       ov.addSubview(year_label = Detail::MovieYearView.initWithMovie(@movie, imageView))
       ov.addSubview(rated_label = Detail::MovieRatedView.initWithMovie(@movie, year_label))
       ov.addSubview(genre_label = Detail::MovieGenreView.initWithMovie(@movie, rated_label))
-      ov.addSubview(directors_label = Detail::MovieGenreView.initWithMovie(@movie, genre_label))
-      ov.addSubview(actor_label = Detail::MovieGenreView.initWithMovie(@movie, directors_label))
-      ov.addSubview(plot_label = Detail::MovieGenreView.initWithMovie(@movie, actor_label))
+      ov.addSubview(directors_label = Detail::MovieDirectorView.initWithMovie(@movie, genre_label))
+      ov.addSubview(actor_label = Detail::MovieActorView.initWithMovie(@movie, directors_label))
+      ov.addSubview(plot_label = Detail::MoviePlotView.initWithMovie(@movie, actor_label))
       ov.addSubview(Detail::SeenItButtonView.initWithMovie(@movie, controller, plot_label))
     end
 
